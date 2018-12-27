40a928bae7dae5e8a9c6dee7308e0263fc0ef6a3
chavali, jayachandra <jayachandra.chavali1@t-mobile.com>
2018-12-27 13:40:03 -0800

    removed the PCF folders for now and updates the e2e spc with an empty

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,21 +1,2 @@
-import { AppPage } from './app.po';
-
-describe('workspace-project App', () => {
-  let page: AppPage;
-
-  beforeEach(() => {
-    page = new AppPage();
-  });
-
-  it('Test suite info first tab to be Report', () => {
-    page.navigateTo();
-  });
-
-  it('should show item when you click on it', function() {
-    page.clickOnMenuItem('Configuration');
-  });
-
-  it('testCase page should open on click of test case link', function() {
-    page.clickOnTestCaseLink('17849');
-  });
-});
+/*
+This is file to have the e2e test cases*/
